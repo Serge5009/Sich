5856d4476273ac44a5a4b17ecdc05be2f5c7864c
Serhii Marchenko <serge5709244@gmail.com>
2024-07-17 17:51:35 -0400

    Added movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    float moveSensitivity = 100.0f;
-    float mouseSensitivity = 100.0f;
+    public float moveSensitivity = 100.0f;
+    public float moveSmoothingSpeed = 1.0f;
+    public float mouseSensitivity = 100.0f;
 
+
+    Vector3 moveSpeed;
+    Vector3 moveTargetSpeed;
     void Start()
     {
         
@@ -14,6 +18,28 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        
+        UpdateMove();
+    }
+
+    void UpdateMove()
+    {
+
+        //  Use keyboard to set the axis
+        moveTargetSpeed = new();
+        if (Input.GetKey(KeyCode.W))
+            moveTargetSpeed.z += 1.0f;
+        if (Input.GetKey(KeyCode.S))
+            moveTargetSpeed.z -= 1.0f;
+        if (Input.GetKey(KeyCode.D))
+            moveTargetSpeed.x += 1.0f;
+        if (Input.GetKey(KeyCode.A))
+            moveTargetSpeed.x -= 1.0f;
+
+        //  Lerp camera speed to the target
+        moveSpeed = Vector3.Lerp(moveSpeed, moveTargetSpeed * moveSensitivity, moveSmoothingSpeed * Time.deltaTime);
+
+        //  Apply speed
+        transform.position += moveSpeed * Time.deltaTime;
+
     }
 }
