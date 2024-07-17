3ed5f1b7323691976fb443acb5f1dbbf70ae2c8c
Serhii Marchenko <serge5709244@gmail.com>
2024-07-17 17:41:53 -0400

    Added camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    float moveSensitivity = 100.0f;
+    float mouseSensitivity = 100.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
